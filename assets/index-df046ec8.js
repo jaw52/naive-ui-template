07d471f517dfaf7948f6d04f71225d666b35ed04
jaw52 <jaw52@users.noreply.github.com>
2023-01-18 02:19:53 +0000

    deploy: b9d5915db250117d61a5cf92fe98690db6869644

---
+++ assets/index-df046ec8.js
@@ -0,0 +1,1 @@
+import{p as e}from"./index-139d7e9f.js";import{J as r,K as n}from"./vue-d1235445.js";const o={};function t(c,s){return r(),n("div",null,"SeniorList")}const _=e(o,[["render",t]]);export{_ as default};
